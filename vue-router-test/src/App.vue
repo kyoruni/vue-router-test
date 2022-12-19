b90259d97a2f927b87b3b1aeaf83ca3b39381ea9
kyoruni <40832190+kyoruni@users.noreply.github.com>
2022-12-19 22:50:02 +0900

    :wrench: トップページ表示

--- vue-router-test/src/App.vue
+++ vue-router-test/src/App.vue
@@ -1,11 +1,48 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { reactive, provide } from 'vue';
+import { RouterView } from 'vue-router';
+import type { Pokemon } from '@/interfaces';
+
+const pokemons = new Map<number, Pokemon>();
+
+const initPokemons = () => {
+  pokemons.set(1, {
+    id: 1,
+    name: "フシギダネ",
+    type1: "くさ",
+    type2: "どく",
+    species: "たね",
+  });
+  pokemons.set(4, {
+    id: 4,
+    name: "ヒトカゲ",
+    type1: "ほのお",
+    species: "とかげ",
+  });
+  pokemons.set(7, {
+    id: 7,
+    name: "ゼニガメ",
+    type1: "みず",
+    species: "かめのこ",
+  });
+};
+initPokemons();
+provide("pokemons", reactive(pokemons));
 </script>
 
 <template>
-  <header>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
-  </header>
-  <RouterView />
+  <div id="app">
+    <h1>vue-router-test</h1>
+    {{  pokemons  }}
+    <main>
+      <RouterView />
+    </main>
+  </div>
 </template>
+
+<style scoped>
+main {
+  border: 1px solid red;
+  padding: 20px;
+}
+</style>
---
+++ vue-router-test/src/views/TopPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="top-page">
+    <h2>TopPage.vue</h2>
+    <nav id="breadcrumbs">
+      <ul>
+        <li>TOP</li>
+      </ul>
+    </nav>
+    <section>
+      <p>管理ページ</p>
+    </section>
+  </div>
+</template>
