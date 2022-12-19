288dbdfdcac52f3da1cd292375e886b8b0674e57
kyoruni <40832190+kyoruni@users.noreply.github.com>
2022-12-19 23:07:57 +0900

    :wrench: 一覧を表示

--- vue-router-test/src/views/PokemonList.vue
+++ vue-router-test/src/views/PokemonList.vue
@@ -1,8 +1,31 @@
+<script setup lang="ts">
+import { inject } from 'vue';
+import { RouterLink } from 'vue-router';
+import type { Pokemon } from '@/interfaces';
+
+const pokemons = inject('pokemons') as Map<number, Pokemon>;
+</script>
+
 <template>
   <div class="pokemon-list">
     <h2>一覧ページ</h2>
+    <nav id="breadcrumbs">
+      <ul>
+        <li>
+          <RouterLink :to="{ name: 'TopPage' }">TOP</RouterLink>
+        </li>
+        <li>
+          ポケモン一覧
+        </li>
+      </ul>
+    </nav>
     <section>
       <h3>ポケモン一覧</h3>
+      <ul>
+        <li v-for="[id, pokemon] in pokemons" :key="id">
+          ID: {{ id }} {{ pokemon.name }}
+        </li>
+      </ul>
     </section>
   </div>
 </template>
